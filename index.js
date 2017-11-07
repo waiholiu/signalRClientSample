fc1adee56ecbae9e7205255ab058aa78e16cd4cb
waiholiu <waiholiuEmail>
2017-11-07 16:27:28 +1100

    last changes

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-alert('hellko');
+var connection = $.hubConnection()
+connection.url = 'http://localhost:56525/signalr';
+var myHubProxy = connection.createHubProxy('myHub');
+myHubProxy.on('announce', function(msg) { console.log(msg)});
+connection.start().done(function(){ console.log('Now connected, connection ID=' + connection.id); }).fail(function(){ console.log('Could not connect'); });
